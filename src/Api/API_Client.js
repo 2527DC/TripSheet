8d0f3f0665c8c432aad96668a41322c0a9911bfe
Chethan <chethan962089@gmail.com>
2025-02-12 12:44:46 +0530

    updated the Apito backend

--- src/Api/API_Client.js
+++ src/Api/API_Client.js
@@ -1,18 +1,16 @@
 import axios from "axios";
 // import Cookies from "js-cookie";
 
+// export const LocalClient = axios.create({
+//   baseURL: "http://localhost:3000/api/",
+// });
+
+
 export const LocalClient = axios.create({
-  baseURL: "http://localhost:3000/api/",
+  baseURL: "https://api.mltcorporate.com/api/",
 });
 
 
-// export const LocalClient = axios.create({
-//   baseURL: "http://194.238.18.112:3000/api/",
-// });
 
-// export const axiosClient = axios.create({
-//   baseURL: "http://194.238.18.112:3000/api/",
-// });
-
- export const imageUrl = "http://localhost:3000/api/get-signature/";
-// export const imageUrl = "https://api.mltcorporate.com/api/get-signature/";
+//  export const imageUrl = "http://localhost:3000/api/get-signature/";
+export const imageUrl = "https://api.mltcorporate.com/api/get-signature/";
